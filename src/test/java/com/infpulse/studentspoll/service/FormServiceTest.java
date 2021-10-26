3207644773136a5e630efdc1576e230958dffbda
Blender <blender17@i.ua>
2021-10-26 14:50:20 +0300

    Fixed form service test, added tests for user answer service

--- src/test/java/com/infpulse/studentspoll/service/FormServiceTest.java
+++ src/test/java/com/infpulse/studentspoll/service/FormServiceTest.java
@@ -2,7 +2,6 @@ package com.infpulse.studentspoll.service;
 
 import com.infpulse.studentspoll.exceptions.NoPermissionException;
 import com.infpulse.studentspoll.exceptions.NotFoundException;
-import com.infpulse.studentspoll.model.entity.Block;
 import com.infpulse.studentspoll.model.entity.Form;
 import com.infpulse.studentspoll.model.entity.User;
 import com.infpulse.studentspoll.model.formDto.FormDto;
@@ -10,19 +9,21 @@ import com.infpulse.studentspoll.repository.FormsRepository;
 import com.infpulse.studentspoll.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.mockito.internal.matchers.Equality.areEqual;
 
 @SpringBootTest(classes = FormService.class)
 public class FormServiceTest {
@@ -31,7 +32,7 @@ public class FormServiceTest {
 	protected FormsRepository formsRepository;
 	@MockBean
 	protected UserRepository userRepository;
-	@MockBean
+	@SpyBean
 	protected ModelMapper modelMapper;
 
 	@Autowired
@@ -40,23 +41,17 @@ public class FormServiceTest {
 
 	@Test
 	public void shouldReturnSavedForm() {
-		FormDto formDto = new FormDto();
-		formDto.setTopicName("form name");
-		LocalDateTime localDateTime = LocalDateTime.of(2022, 10, 23, 17, 0);
-		formDto.setExpireDate(localDateTime);
-		formDto.setListOfBlocks(List.of(new Block()));
 		User user = new User();
 		user.setLogin("username");
-		Form form = new Form(1L, formDto.getTopicName(), null, null,
-				null, formDto.getExpireDate(), null, null,
-				formDto.getListOfBlocks(), null);
+		Form form = new Form(1L, "form name", null, null,
+				null, LocalDateTime.of(2022, 10, 23, 17, 0), null, null,
+				null, null);
+		FormDto formDto = modelMapper.map(form, FormDto.class);
 
-		when(modelMapper.map(any(FormDto.class), any())).thenReturn(form);
-		when(modelMapper.map(any(Form.class), any())).thenReturn(formDto);
 		when(userRepository.findByLogin(any())).thenReturn(Optional.of(user));
 		when(formsRepository.save(any())).thenReturn(form);
 
-		assertEquals(formDto, formService.saveForm(formDto, "username"));
+		areEqual(formDto,  formService.saveForm(formDto, "username"));
 	}
 
 	@Test
@@ -70,19 +65,12 @@ public class FormServiceTest {
 		List<Form> forms = List.of(form0, form1, form2);
 		User user = new User();
 		user.setLogin("username");
-		FormDto formDto0 = new FormDto();
-		formDto0.setTopicName(form0.getTopicName());
-		FormDto formDto1 = new FormDto();
-		formDto0.setTopicName(form1.getTopicName());
-		FormDto formDto2 = new FormDto();
-		formDto0.setTopicName(form2.getTopicName());
-		List<FormDto> formDtos = List.of(formDto0, formDto1, formDto2);
+		List<FormDto> formDtos = modelMapper.map(forms, new TypeToken<List<FormDto>>() {}.getType());
 
-		when(modelMapper.map(any(), any())).thenReturn(formDtos);
 		when(userRepository.findByLogin(any())).thenReturn(Optional.of(user));
 		when(formsRepository.findAllByOwner(any())).thenReturn(forms);
 
-		assertEquals(formDtos, formService.getOwnedForms("username"));
+		areEqual(formDtos, formService.getOwnedForms("username"));
 	}
 
 	@Test
@@ -106,15 +94,14 @@ public class FormServiceTest {
 	@Test
 	public void shouldReturnRequestedForm() {
 		Form form = new Form(1L, "form 0", null, null
-				, null, null, null, null, null, null);
+				, null, LocalDateTime.of(2022, 10, 23, 17, 0),
+				null, null, null, null);
 
-		FormDto formDto = new FormDto();
-		formDto.setTopicName(form.getTopicName());
+		FormDto formDto = modelMapper.map(form, FormDto.class);
 
 		when(formsRepository.findById(1L)).thenReturn(Optional.of(form));
-		when(modelMapper.map(any(Form.class), any())).thenReturn(formDto);
 
-		assertEquals(formDto, formService.getForm(1L));
+		areEqual(formDto, formService.getForm(1L));
 	}
 
 }
--- src/test/java/com/infpulse/studentspoll/service/UserAnswerServiceTest.java
+++ src/test/java/com/infpulse/studentspoll/service/UserAnswerServiceTest.java
@@ -1,13 +1,28 @@
 package com.infpulse.studentspoll.service;
 
+import com.infpulse.studentspoll.model.entity.Block;
+import com.infpulse.studentspoll.model.entity.User;
+import com.infpulse.studentspoll.model.entity.UserAnswer;
+import com.infpulse.studentspoll.model.formDto.FormHeader;
+import com.infpulse.studentspoll.model.formDto.UserAnswerDto;
 import com.infpulse.studentspoll.repository.UserAnswerRepository;
 import com.infpulse.studentspoll.repository.UserRepository;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
-@ExtendWith(SpringExtension.class)
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.matchers.Equality.areEqual;
+
+@SpringBootTest(classes = UserAnswerService.class)
 public class UserAnswerServiceTest {
 
 	@MockBean
@@ -16,9 +31,66 @@ public class UserAnswerServiceTest {
 	protected UserRepository userRepository;
 	@MockBean
 	protected FormService formService;
+	@SpyBean
+	protected ModelMapper mapper;
 
-	@InjectMocks
+	@Autowired
 	UserAnswerService answerService;
 
+	@Test
+	public void shouldReturnUserAnswer() {
+		Block block0 = new Block(1L, null, "question 1");
+		Block block1 = new Block(1L, null, "question 2");
+		Block block2 = new Block(1L, null, "question 3");
+		List<Block> blockList = List.of(block0, block1, block2);
+
+		UserAnswer userAnswer = new UserAnswer(1L, null, null
+				, blockList, null);
+
+		UserAnswerDto userAnswerDto = mapper.map(userAnswer, UserAnswerDto.class);
+
+		when(answerRepository.findUserAnswerByBelongsToForm_IdAndAnswerOwner_Login(anyLong(), anyString())).thenReturn(userAnswer);
+
+		areEqual(userAnswerDto, answerService.getUserAnswer(1L, "username"));
+	}
+
+	@Test
+	public void shouldReturnPersistedAnswer() {
+		User user = new User();
+		user.setLogin("username");
+		Block block0 = new Block(1L, null, "question 1");
+		Block block1 = new Block(1L, null, "question 2");
+		Block block2 = new Block(1L, null, "question 3");
+		List<Block> blockList = List.of(block0, block1, block2);
+
+		UserAnswer userAnswer = new UserAnswer(1L, null, null
+				, blockList, null);
+
+		UserAnswerDto userAnswerDto = mapper.map(userAnswer, UserAnswerDto.class);
+
+		when(userRepository.findByLogin(any())).thenReturn(Optional.of(user));
+		when(answerRepository.save(any())).thenReturn(userAnswer);
+		when(formService.getFormEntity(anyLong())).thenReturn(null);
+
+		areEqual(userAnswerDto, answerService.saveAnswer(1L, userAnswerDto, "username"));
+	}
+
+	@Test
+	public void shouldReturnUserAnswers() {
+		User user = new User();
+		user.setLogin("username");
+		FormHeader formHeader0 = new FormHeader("form 1",
+				LocalDateTime.of(2020, 10, 23, 17, 0), 1L);
+		FormHeader formHeader1 = new FormHeader("form 2",
+				LocalDateTime.of(2020, 10, 25, 13, 25), 2L);
+		FormHeader formHeader2 = new FormHeader("form 3",
+				LocalDateTime.of(2020, 10, 29, 19, 56), 3L);
+		List<FormHeader> formHeaders = List.of(formHeader0, formHeader1, formHeader2);
+
+		when(answerRepository.getUserAnswers(any())).thenReturn(formHeaders);
+		when(userRepository.findByLogin(any())).thenReturn(Optional.of(user));
+
+		areEqual(formHeaders, answerService.getUserAnswers("username"));
+	}
 
 }
