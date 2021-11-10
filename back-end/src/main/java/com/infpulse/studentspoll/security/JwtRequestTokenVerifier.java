ee7fc4043d4bf0c6a8963c5486eba44fd6b4451d
Gurmigou <yehor.bukhanets@gmail.com>
2021-11-10 15:21:56 +0200

    unnecessary code fragments removed

--- back-end/src/main/java/com/infpulse/studentspoll/security/JwtRequestTokenVerifier.java
+++ back-end/src/main/java/com/infpulse/studentspoll/security/JwtRequestTokenVerifier.java
@@ -37,8 +37,6 @@ public class JwtRequestTokenVerifier extends OncePerRequestFilter {
             // execute a "replace" method to remove a "Bearer " word
             final String jwtToken = authorizationHeader.replace("Bearer ", "");
 
-
-
             try {
                 String userEmail = jwtProvider.extractUserEmail(jwtToken);
                 Authentication authentication = new UsernamePasswordAuthenticationToken(
