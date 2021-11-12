a182607261d279aa0835e0e7fe55d57013e21c48
Gurmigou <yehor.bukhanets@gmail.com>
2021-11-12 23:52:24 +0200

    front: implemented About page structure

--- front-end/src/component/info/AboutProject.tsx
+++ front-end/src/component/info/AboutProject.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import Layout from "../navbarFooter/Layout";
+import "../../style/aboutProject.css"
 
 function AboutProject() {
     return (
         <Layout>
-            <div className="all-outer-container">
-                About
+            <div className="all-outer-container about">
+                <div className="about-row">
+                    <div className="about-column">
+                        <div className="about-box">
+                            Здесь будет какая-то информация про проект
+                        </div>
+                    </div>
+                    <div className="about-column">
+                        <div className="about-box">
+                            Здесь будет какая-то информация про проект
+                        </div>
+                    </div>
+                </div>
+                <div className="about-row">
+                    <div className="about-column">
+                        <div className="about-box">
+                            Здесь будет какая-то информация про проект
+                        </div>
+                    </div>
+                    <div className="about-column">
+                        <div className="about-box">
+                            Здесь будет какая-то информация про проект
+                        </div>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
