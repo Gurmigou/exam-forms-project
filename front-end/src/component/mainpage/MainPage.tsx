9bd21f82b7dd42d955d4196db89d3a7e2536015d
Gurmigou <yehor.bukhanets@gmail.com>
2021-11-24 00:31:56 +0200

    implemented that you can't access to the app while not authenticated

--- front-end/src/component/mainpage/MainPage.tsx
+++ front-end/src/component/mainpage/MainPage.tsx
@@ -1,17 +1,13 @@
-import React, {useEffect} from "react";
+import React from "react";
 import "../../style/mainPage.css"
 import MainPageComponent from "./MainPageComponent";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import {logout} from "../../utils/redux/reduxUtils";
 import {useDispatch, useSelector} from "react-redux";
 
 function MainPage() {
-    const user = useSelector((state: any) => state)
+    const isAuth = useSelector((state: any) => state.isAuth)
     const dispatch = useDispatch()
-
-    useEffect(() => {
-        console.log(user);
-    }, [])
 
     return (
         <div className="main-page">
@@ -20,7 +16,7 @@ function MainPage() {
                     <h1 className="">Edulse</h1>
                 </Link>
                 {
-                    !user.isAuth ?
+                    !isAuth ?
                         <Link id="main-page-nav-link-2" className="main-page-nav-link" to="/login">
                             <h1 className="">Sign in</h1>
                         </Link>
@@ -32,8 +28,10 @@ function MainPage() {
             </div>
             <div className="main-page-components-box">
                 <div className="main-page-components-box-inner">
-                    <MainPageComponent title={"New form"} index={1} buttonText={"Create"} onClickRoute={"/form/construct"}/>
-                    <MainPageComponent title={"Form list"} index={2} buttonText={"View"} onClickRoute={"/user/form-list"}/>
+                    <MainPageComponent title={"New form"} index={1} buttonText={"Create"}
+                                       onClickRoute={isAuth ? "/form/construct" : "/login"}/>
+                    <MainPageComponent title={"Form list"} index={2} buttonText={"View"}
+                                       onClickRoute={isAuth ? "/user/form-list" : "/login"}/>
                 </div>
             </div>
         </div>
